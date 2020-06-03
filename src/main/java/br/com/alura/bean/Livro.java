3356227049e3ba43fa19448602163ef73788ecd7
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-06-03 18:54:03 -0300

    Configuração do Ambiente e Primeiros Passos com JSF

---
+++ src/main/java/br/com/alura/bean/Livro.java
@@ -0,0 +1,42 @@
+package br.com.alura.bean;
+
+public class Livro {
+
+	public String titulo;
+	public String isbn;
+	public double preco;
+	public String dataLancamento;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(String dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+}
