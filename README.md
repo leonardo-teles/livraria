fa979d95aaacce0700ef65f8979a821a62d3713d
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-06-17 02:49:40 -0300

    Logout e o Escopo Flash

---
+++ README.md
@@ -0,0 +1,20 @@
+# Java e JSF I: Sua aplicação web com JSF2 
+
+Tópicos Abordados:
+
+  - Introdução ao desenvolvimento web com JSF
+  - Configuração do ambiente e os primeiros passos com JSF
+  - Entendo MVC e integrando o banco de dados com JPA 2
+  - Completando o sistema e lidando com escopos do JSF 2
+  - Conversão e validação de dados
+  - Páginas mais dinâmicas com AJAX
+  - Navegação e ciclo de vida dos componentes JSF
+  - Templates reutilizáveis com Facelets
+  - Completando o CRUD
+  - Atualizando para o JSF 2.2
+  - Autenticação do usuário
+  - Autorização pelo PhaseListener
+  - Logout e o escopo Flash
+
+Certificados: <a href="https://cursos.alura.com.br/certificate/735a5cc9-4314-4531-88a7-f9348cf2763b" rel="noopener" targer="_blank">I</a>
+  
--- src/main/java/br/com/alura/bean/LoginBean.java
+++ src/main/java/br/com/alura/bean/LoginBean.java
@@ -1,5 +1,6 @@
 package br.com.alura.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -14,17 +15,26 @@ public class LoginBean {
 	private Usuario usuario = new Usuario();
 	
 	public String efetuarLogin() {
-		System.out.println("efetuando login do usuário " + this.usuario.getEmail());
+		FacesContext context = FacesContext.getCurrentInstance();
 		
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		if (existe) {
-			FacesContext context = FacesContext.getCurrentInstance();
 			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			
 			return "livro?faces-redirect=true";
 		}
+
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
 		
-		return null;
+		return "login?faces-redirect=true";
+	}
+	
+	public String deslogar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		
+		return "login?faces-redirect=true";
 	}
 
 	public Usuario getUsuario() {
